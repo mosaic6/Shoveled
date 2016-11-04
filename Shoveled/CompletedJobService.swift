ff2485b306ca7f14088d1b91dc73778bb3806335
Joshua Walsh <mosaic6@mac.com>
2016-11-03 22:35:34 -0400

    Updates stripe payments and authorized accounts

---
+++ Shoveled/CompletedJobService.swift
@@ -0,0 +1,9 @@
+//
+//  CompletedJobService.swift
+//  Shoveled
+//
+//  Created by Joshua Walsh on 11/2/16.
+//  Copyright © 2016 Lucky Penguin. All rights reserved.
+//
+
+import Foundation
---
+++ Shoveled/EmailService.swift
@@ -0,0 +1,9 @@
+//
+//  EmailService.swift
+//  Shoveled
+//
+//  Created by Joshua Walsh on 10/31/16.
+//  Copyright © 2016 Lucky Penguin. All rights reserved.
+//
+
+import Foundation
