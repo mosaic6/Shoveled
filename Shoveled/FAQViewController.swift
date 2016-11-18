db8b3e4b7475b78902c2f0eda1604c183f56609a
Joshua Walsh <mosaic6@mac.com>
2016-11-18 07:41:27 -0500

    Adds FAQ page

---
+++ Shoveled/FAQViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FAQViewController.swift
+//  Shoveled
+//
+//  Created by Joshua Walsh on 11/17/16.
+//  Copyright © 2016 Lucky Penguin. All rights reserved.
+//
+
+import UIKit
+
+class FAQViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
