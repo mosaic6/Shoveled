6fec79bee4619a4d9360d61551f638fea366547e
Joshua Walsh <mosaic6@mac.com>
2016-12-19 10:06:57 -0500

    Update AppDelegate.swift

--- Shoveled/AppDelegate.swift
+++ Shoveled/AppDelegate.swift
@@ -43,7 +43,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // TODO: Replace with your own test publishable key
         // TODO: DEBUG ONLY! Remove / conditionalize before launch
 //        Stripe.setDefaultPublishableKey("pk_test_sInJmSxsoYOl5rPAv45pvwCv")
-        Stripe.setDefaultPublishableKey("pk_live_xvZp8nbvhuCB3pIrykXwZOEn")
+        Stripe.setDefaultPublishableKey("///")
 
         // [START register_for_notifications]
         if #available(iOS 10.0, *) {
